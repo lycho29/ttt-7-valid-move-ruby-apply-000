453aa4d23c4e2f4be5eb0df7282ea14f495999ac
Lydia Cho <lydiacho@gse.havard.edu>
2020-09-09 15:40:19 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,8 +11,8 @@ end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
   if !(board[index] == " " || board[index] == "" || board[index] == nil)
-     false
+     return false
   else
-     true
+     return true
   end
 end
