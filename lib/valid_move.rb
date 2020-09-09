e27cd80e8de0953cb9f46e6a6436939ddf1e3d94
Lydia Cho <lydiacho@gse.havard.edu>
2020-09-09 15:33:52 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,10 +1,10 @@
 # code your #valid_move? method here
 
 def valid_move?(board, position)
-  if position.to_i.between?(1,9)
-    if !position_taken?(board, position.to_i-1)
-    else
+    if !position_taken?(board, position.to_i-1) && position.to_i.between?(0,8)
       true
+    else
+      false
     end
  end
 
